109ca7afbeb0d1cf80bf62d5e2deb6fd11e78e4f
Tifannie Truman <nietru143@gmail.com>
2023-05-29 17:37:14 -0400

    created clientside code in mutations queries appjs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,21 @@
 //  TODO:
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      password
+      savedBooks {
+        bookId
+        title
+        [Author]
+        description
+        Image
+        link
+      }
+    }
+  }
+`;
