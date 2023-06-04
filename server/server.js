8dd63665a57920ddb2be29dfd2663dc201641833
Tifannie Truman <nietru143@gmail.com>
2023-06-04 17:40:50 -0400

    minor changes for trying to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -3,20 +3,21 @@ const path = require("path");
 const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const mongoose = require("mongoose");
 
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Comment out this code once you have built out queries and mutations in the client folder
-// const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Book-Search"
+);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -26,25 +27,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-// New instance of apollo server
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
-
-  // db accesses our models via mongoose ./config/connection.js
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
-  });
 };
-
-// Uncomment the following code once you have built the queries and mutations in the client folder
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+// Call the async function to start the server
 startApolloServer();
