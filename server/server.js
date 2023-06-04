1c63f062c44350469f3e03a7ab83b8cf1bd5aabf
Tifannie Truman <nietru143@gmail.com>
2023-06-04 19:39:39 -0400

    fixed schemas

--- server/server.js
+++ server/server.js
@@ -22,26 +22,7 @@ mongoose.connect(
 const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://nietru143:Poopdick09!@cluster0.hljf9b5.mongodb.net/googlebooks?retryWrites=true&w=majority";
-// Create a new MongoClient
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// The current database to use.
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
